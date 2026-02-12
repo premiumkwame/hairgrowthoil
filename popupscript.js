578379cec04d2a2ee5547492c5e6e7ed74b25dd8
premiumkwame <kransly007@gmail.com>
2026-02-12 11:53:18 +0000

    commit

--- popupscript.js
+++ popupscript.js
@@ -1,7 +1,7 @@
 // show popup after 5 seconds
 setTimeout(function(){
     document.getElementById("email-popup").style.display ="flex"
-}, 5000);
+},5000);
 
 // close popup when X is clicked
 document.querySelector(".close-btn").addEventListener("click",function(){
