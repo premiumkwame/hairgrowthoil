da205dd204903eed6b9c98e1d1f6722c5c414b26
premiumkwame <kransly007@gmail.com>
2026-02-17 12:36:43 +0000

    commit

--- productpopupscript.js
+++ productpopupscript.js
@@ -1,7 +1,7 @@
-// show popup after 5 seconds
+// show popup after 2 seconds
 setTimeout(function(){
     document.getElementById("mc_embed_shell").style.display ="flex"
-},5000);
+},2000);
 
 // close popup when X is clicked
 document.querySelector(".close-btn").addEventListener("click",function(){
